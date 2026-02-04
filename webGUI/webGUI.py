c99e97142d42c9d572bf06fa57f173b4d44d4fc5
xuezuoys <account@xuezuo.xyz>
2026-02-05 01:34:56 +0800

    初步增加网页UI

---
+++ webGUI/webGUI.py
@@ -0,0 +1,28 @@
+
+import json
+from flask import Flask, render_template, request
+
+from src import password_generate
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def generate_password():
+    json_data = request.get_json()
+    if not json_data: return 'No input data provided'
+
+    length = int(json_data['length'])
+    special_chars = json_data['special_chars']
+    include_numbers = json_data['include_numbers']
+    allow_start_with_special = json_data['allow_start_with_special']
+
+    password = password_generate.generator(length, special_chars, include_numbers, allow_start_with_special)
+
+    return password
+
+def run():
+    app.run(debug=False)
---
+++ webGUIrun.py
@@ -0,0 +1,3 @@
+from webGUI import webGUI
+
+webGUI.run()
